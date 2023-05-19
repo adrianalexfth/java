8f6346651ee5f1aed729e4102114451de230cf00
adrianalexfth <134097093+adrianalexfth@users.noreply.github.com>
2023-05-19 21:44:41 +0200

    Create GitHubRawPageReader.java

---
+++ githubrawpagereader/GitHubRawPageReader.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GitHubRawPageReader {
+    public static void main(String[] args) {
+        String rawPageUrl = "https://raw.githubusercontent.com/username/repo/master/file.txt";
+
+        try {
+            URL url = new URL(rawPageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder content = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                }
+                reader.close();
+
+                System.out.println(content.toString());
+            } else {
+                System.out.println("Error: " + responseCode);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
